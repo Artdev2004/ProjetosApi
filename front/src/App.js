9ee71d02ee2a0072dd507bbe8d19b3be915c7efd
Arthur S. Nascimento <arthur.soares.nascimento31@gmail.com>
2023-04-26 15:54:52 -0300

    Subindo ultimas funções da API

--- front/src/App.js
+++ front/src/App.js
@@ -1,13 +1,141 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
 import Formulario from './Formulario';
+import Tabela from './Tabela';
 
 function App() {
+
+  // Objeto produto 
+const produto = {
+  codigo : 0,
+  nome : '',
+  marca : ''
+}
+
+  // UseState 
+  const [produtos, setProdutos] = useState([])
+  const [btnCadastrar, setBtnCadastrar] = useState(true)
+  const [objProduto, setObjProduto] = useState(produto)
+
+  console.log(objProduto)
+
+  // UseEffect 
+  useEffect(()=>{
+    fetch("http://localhost:8080/listar")
+    .then(retorno => retorno.json())
+    .then(retorno_convertido => setProdutos(retorno_convertido));
+  }, []);
+
+
+  //Obtendo os dados do fomrulário
+  const aoDigitar = (e) => {
+    setObjProduto({...objProduto, [e.target.name]:e.target.value});
+  }
+
+  // Cadastrar Produto
+  const cadastrar = () => {
+    fetch('http://localhost:8080/cadastrar',{
+      method:'post',
+      body:JSON.stringify(objProduto),
+      headers:{
+        'Content-type':'application/json',
+        'Accept':'application/json'
+      }
+    })
+    .then(retorno => retorno.json())
+    .then(retorno_convertido => {
+      retorno_convertido.mensagem !== undefined ? alert(retorno_convertido.mensagem) : setProdutos([...produtos, retorno_convertido]);
+      alert("Cadastro Realizado com Sucesso")
+      limparFormulario()
+    })
+  }
+
+  // Limpar Fomulario
+
+  const limparFormulario = () => {
+    setObjProduto(produto)
+    setBtnCadastrar(true);
+  }
+
+  // Selecionar Produto
+  const selecionarProduto = (indice) =>{
+    setObjProduto(produtos[indice]);
+    setBtnCadastrar(false);
+  }
+
+  // Deletar Produto
+  const deletarProduto = () => {
+    fetch('http://localhost:8080/deletar/'+objProduto.codigo,{
+      method:'delete',
+      headers:{
+        'Content-type':'application/json',
+        'Accept':'application/json'
+      }
+    })
+    .then(retorno => retorno.json())
+    .then(retorno_convertido => {
+      // Mensagem 
+      alert(retorno_convertido.mensagem);
+
+
+      // Cópia do Vetor de Produtos
+
+      let vetorTemp = [...produtos];
+
+      //Variavel de Indice
+      let indice = vetorTemp.findIndex((p) =>{
+        return p.codigo === objProduto.codigo;
+
+      });
+
+      //Remover produto do vetor Temporario
+      vetorTemp.splice(indice, 1);
+
+      //Atualizar o vetor de produtos
+      setProdutos(vetorTemp);
+
+      // Limpar Formulario
+      limparFormulario();
+    })
+  }
+  
+  // Alterar Produto 
+  const atualizarProduto = () => {
+    fetch('http://localhost:8080/alterar',{
+      method:'put',
+      body:JSON.stringify(objProduto),
+      headers:{
+        'Content-type':'application/json',
+        'Accept':'application/json'
+      }
+    })
+    .then(retorno => retorno.json())
+    .then(retorno_convertido => {
+      retorno_convertido.mensagem !== undefined ? alert(retorno_convertido.mensagem) : 
+      alert("Produto Alterado com Sucesso");
+
+      // Cópia do Vetor de Produtos
+      let vetorTemp = [...produtos];
+
+      //Variavel de Indice
+      let indice = vetorTemp.findIndex((p) =>{
+        return p.codigo === objProduto.codigo;
+      });
+
+      //Alterar produto do vetor Temporario
+      vetorTemp[indice] = objProduto;
+
+      //Atualizar o vetor de produtos
+      setProdutos(vetorTemp);
+      limparFormulario()
+    })
+  }
+  //Retorno
   return (
-    <div className="App">
-      <Formulario>
-        
-      </Formulario>
+    <div>
+      <Formulario botao={btnCadastrar} eventoTeclado={aoDigitar} cadastrar={cadastrar} obj={objProduto} cancelar={limparFormulario} deletar={deletarProduto} alterar={atualizarProduto}/>
+      <Tabela vetor={produtos} selecionar={selecionarProduto}/>
     </div>
   );
 }
--- front/src/Formulario.js
+++ front/src/Formulario.js
@@ -1,6 +1,23 @@
-function Formulario(){
+function Formulario({botao, eventoTeclado, cadastrar, obj, cancelar, deletar, alterar}){
     return(
-        <h1>Formulário</h1>
+        <form>
+            <input type="text" value={obj.nome} onChange={eventoTeclado} name='nome' placeholder="Nome" id="form-control-nome" className="form-control" />
+            <input type="text" value={obj.marca}onChange={eventoTeclado} name='marca' placeholder="Marca" className="form-control" />
+
+            {
+                botao
+                ?
+                <input type="button" value="Cadastrar"  onClick={cadastrar} className="btn btn-primary"/>
+                :
+                <div>
+                    <input type="button" onClick={alterar} value="Atualizar" className="btn btn-warning" />
+                    <input type="button" onClick={deletar} value="Deletar" className="btn btn-danger" />
+                    <input type="button" onClick={cancelar} value="Cancelar" className="btn btn-secondary" />
+                </div>
+            }
+           
+            
+        </form>
     )
 }
 
